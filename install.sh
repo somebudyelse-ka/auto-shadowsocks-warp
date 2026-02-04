c65dede2ff0437dffcefc0d044a2f426e6a19c3e
somebudyelse-ka <somebudyelse@proton.me>
2026-02-03 16:19:18 -0800

    Automatically accept tos

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmo
 echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 sudo apt-get update && sudo apt-get install -y cloudflare-warp
 # End adapted code
-warp-cli registration new
+yes | warp-cli registration new
 warp-cli mode proxy
 warp-cli connect
 sudo sed -i 's/\bsocks4\b/socks5/g; s/\b9050\b/40000/g' /etc/proxychains.conf
