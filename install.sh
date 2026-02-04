4350f4d4615ad9b928c877d126f1dd417c5edab4
somebudyelse-ka <somebudyelse@proton.me>
2026-02-03 16:16:22 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt update && sudo apt install -y gnupg2 shadowsocks-libev proxychains4
+# Start adapted code from pkg.cloudflareclient.com
+curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
+sudo apt-get update && sudo apt-get install -y cloudflare-warp
+# End adapted code
+warp-cli registration new
+warp-cli mode proxy
+warp-cli connect
+sudo sed -i 's/\bsocks4\b/socks5/g; s/\b9050\b/40000/g' /etc/proxychains.conf
+sudo sed -i 's|^ExecStart=|ExecStart=/usr/bin/proxychains4 |' /lib/systemd/system/shadowsocks-libev.service
+sudo systemctl daemon-reload && sudo systemctl restart shadowsocks-libev
